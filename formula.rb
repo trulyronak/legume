4b50563daa405beb5a9a9a95dfd4f2b07695c48f
Bryce Tsuyuki <btsuyuki1@gmail.com>
2020-05-24 23:29:15 -0700

    Fix issue #2
    Moved version above url and changed hardcoded version in url to #{version}

--- formula.rb
+++ formula.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Legume < Formula
   desc "Professor Copperfield's Miracle Legumes will help you do anything"
   homepage "https://github.com/trulyronak/legume"
-  url "https://registry.npmjs.org/legume/-/legume-1.0.0.tgz"
-  version "1.0.0"
+  version "2.0.0"
+  url "https://registry.npmjs.org/legume/-/legume-#{version}.tgz"
   sha256 "14e7f02ce12bdbbb78834ef22e6597356973333e3770093beed64c598cde4706"
 
   depends_on "node"
