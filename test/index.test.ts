bc29dde6169037ca7a17429c4f6bcd70fb1e9156
Ronak Shah <ronakshahultimate@gmail.com>
2020-05-24 20:16:22 -0700

    fixed ishaans code

--- test/index.test.ts
+++ test/index.test.ts
@@ -7,13 +7,6 @@ describe('test', () => {
   .stdout()
   .do(() => cmd.run([]))
   .it('runs hello', ctx => {
-    expect(ctx.stdout).to.contain('hello world')
-  })
-
-  test
-  .stdout()
-  .do(() => cmd.run(['--name', 'jeff']))
-  .it('runs hello --name jeff', ctx => {
-    expect(ctx.stdout).to.contain('hello jeff')
+    expect(ctx.stdout).to.contain('Jim')
   })
 })
